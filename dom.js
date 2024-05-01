a85166b4b376dfd7d3aa7c17078dc85d1012f92d
Adeelbhatti11 <adeelbhattiab1232gmail.com>
2024-05-01 19:06:49 +0500

    first commit

---
+++ dom.js
@@ -0,0 +1,9 @@
+var element;
+element = document;
+console.log(element);
+var elements;
+elements = document.head;
+console.log(elements);
+var elemnt;
+elemnt = document.links;
+console.log(elemnt);
